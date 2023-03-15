21ffcfe82879b645eea91bb3c355d6dbe400bf54
Siigmaf <faby.tbe@gmail.com>
2023-03-15 17:50:45 -0300

    desafio10

---
+++ challenges/desafio10.js
@@ -0,0 +1,10 @@
+db.produtos.find(
+    {
+        valoresNutricionais: {
+            $elemMatch: { 
+                tipo: "proteínas", percentual: { $gte: 30, $lte: 40 }, 
+            },
+        },
+    },
+    { nome: 1, _id: 0 },
+);
